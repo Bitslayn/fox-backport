0dc635945e8a7593f9addf4affdb91f35336cf34
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2026-01-16 22:26:24 -0800

    render-task-light-overlay-fix
    and light-crash-fix, squashed
    1.20.1

---
+++ common/src/main/java/bitslayn/backport/mixin/ImmediateAvatarRendererMixin.java
@@ -0,0 +1,63 @@
+package bitslayn.backport.mixin;
+
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalIntRef;
+import net.minecraft.client.renderer.LightTexture;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import org.figuramc.figura.math.vector.FiguraVec3;
+import org.figuramc.figura.model.FiguraModelPart;
+import org.figuramc.figura.model.PartCustomization;
+import org.figuramc.figura.model.rendering.ImmediateAvatarRenderer;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ImmediateAvatarRenderer.class)
+public class ImmediateAvatarRendererMixin {
+    @Shadow
+    @Final
+    protected PartCustomization.PartCustomizationStack customizationStack;
+
+    @Definition(id = "getPivot", method = "Lorg/figuramc/figura/model/PartCustomization;getPivot()Lorg/figuramc/figura/math/vector/FiguraVec3;")
+    @Definition(id = "add", method = "Lorg/figuramc/figura/math/vector/FiguraVec3;add(Lorg/figuramc/figura/math/vector/FiguraVec3;)Lorg/figuramc/figura/math/vector/FiguraVec3;")
+    @Definition(id = "pivot", local = @Local(type = FiguraVec3.class, name = "pivot"))
+    @Expression("pivot = ?.getPivot().?().add(?)")
+    @Inject(method = "renderPart", at = @At("MIXINEXTRAS:EXPRESSION"))
+    private void preCalculateLight(FiguraModelPart part,
+                                   int[] remainingComplexity,
+                                   boolean prevPredicate,
+                                   CallbackInfoReturnable<Boolean> cir,
+                                   @Share("preLight") LocalIntRef preLight,
+                                   @Share("preOverlay") LocalIntRef preOverlay) {
+        PartCustomization oldPeek = customizationStack.peek();
+        preLight.set(oldPeek.light != null ? oldPeek.light : LightTexture.FULL_BRIGHT);
+        preOverlay.set(oldPeek.overlay != null ? oldPeek.overlay : OverlayTexture.NO_OVERLAY);
+    }
+
+    @Inject(
+            method = "renderPart",
+            at = @At(value = "INVOKE", target = "Ljava/util/Collection;iterator()Ljava/util/Iterator;"),
+            slice = @Slice(
+                    from = @At(value = "INVOKE", target = "Lorg/figuramc/figura/model/rendering/ImmediateAvatarRenderer;renderPivot(Lorg/figuramc/figura/model/FiguraModelPart;Lorg/figuramc/figura/model/PartCustomization;)V"),
+                    to = @At(value = "INVOKE", target = "Lorg/figuramc/figura/model/rendering/ImmediateAvatarRenderer;savePivotTransform(Lorg/figuramc/figura/model/ParentType;Lorg/figuramc/figura/model/PartCustomization;)V")
+            )
+    )
+    private void usePreCalculatedLight(FiguraModelPart part,
+                                       int[] remainingComplexity,
+                                       boolean prevPredicate,
+                                       CallbackInfoReturnable<Boolean> cir,
+                                       @Local(name = "light") LocalIntRef light,
+                                       @Local(name = "overlay") LocalIntRef overlay,
+                                       @Share("preLight") LocalIntRef preLight,
+                                       @Share("preOverlay") LocalIntRef preOverlay) {
+        light.set(preLight.get());
+        overlay.set(preOverlay.get());
+    }
+}
